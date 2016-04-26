a90bf750a01499d4ee6e93cd9804695edda8bac6
Normand Martin <normandmartin@macbook-pro-de-normand-martin.local>
2016-04-26 13:24:24 -0400

    Initial Commit

---
+++ HitoryTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HitoryTest
+//
+//  Created by Normand Martin on 16-04-26.
+//  Copyright © 2016 Normand Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
