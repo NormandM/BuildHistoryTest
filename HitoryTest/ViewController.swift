69b2fc2c87612841f1225c2c66e369032250345a
Normand Martin <normandmartin@macbook-pro-de-normand-martin.local>
2016-04-28 06:28:34 -0400

    Pour la création de Plist

--- HitoryTest/ViewController.swift
+++ HitoryTest/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var arr: NSMutableArray = []
+    var n: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        if let plist = Plist(name: "HumanPrehistory") {
+            arr = plist.getMutablePlistFile()!
+        }
+        for arrs in arr {
+            let elementHistory = arrs as! [String]
+                var dateElementArray = elementHistory[0].componentsSeparatedByString(": ")
+                print(dateElementArray)
+                dateElementArray.append("0")
+                dateElementArray.append("0")
+                arr[n] = dateElementArray
+                print(dateElementArray)
+            n = n + 1
+        }
+        if let plist = Plist(name: "HumanPrehistory"){
+            do {
+                try plist.addValuesToPlistFile(arr)
+            } catch {
+                print(error)
+            }
+            //let newArr = plist.getValuesInPlistFile()!
+            //print(newArr)
+        }else{
+            print("unable to get plist")
+        }
+        
 
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
